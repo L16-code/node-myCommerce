bcc814be1770e31661898052cb0a9a5fe0c131a4
L16-code <72161238+L16-code@users.noreply.github.com>
2024-07-12 15:31:17 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,30 +7,30 @@ const app= express();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 import cors from "cors"
-import PermissionRouter from "./features/admin/permissions/routes";
-import RoleRouter from "./features/admin/roles/routes";
-import UserRouter from "./features/admin/users/routes";
-import ProductCategoryRouter from "./features/admin/products-category/routes";
-import ProductRouter from "./features/admin/products/routes";
-import UserPanelRouter from "./features/user/auth/routes";
-import UserProductRouter from "./features/user/products/routes";
+// import PermissionRouter from "./features/admin/permissions/routes";
+// import RoleRouter from "./features/admin/roles/routes";
+// import UserRouter from "./features/admin/users/routes";
+// import ProductCategoryRouter from "./features/admin/products-category/routes";
+// import ProductRouter from "./features/admin/products/routes";
+// import UserPanelRouter from "./features/user/auth/routes";
+// import UserProductRouter from "./features/user/products/routes";
 // app.use(express.json());
 const env =envConfig();
-const port=env.port;
+const port=5000;
 connectDB()
 app.use(cors({
     origin:"*", 
     credentials: true 
 }));
 
-app.use("/permission",PermissionRouter );
-app.use("/roles",RoleRouter );
-app.use("/user",UserRouter);
-app.use("/product-category",ProductCategoryRouter);
-app.use("/product",ProductRouter);
+// app.use("/permission",PermissionRouter );
+// app.use("/roles",RoleRouter );
+// app.use("/user",UserRouter);
+// app.use("/product-category",ProductCategoryRouter);
+// app.use("/product",ProductRouter);
 
 // user-panel routes
-app.use('/',UserPanelRouter,UserProductRouter)
+// app.use('/',UserPanelRouter,UserProductRouter)
 app.listen(port,()=>{
     console.log("server is running on port http://localhost:"+port);
 })

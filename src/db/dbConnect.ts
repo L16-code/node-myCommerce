de373a036436d4958c2aab3fa37cf615f1207df0
L16-code <72161238+L16-code@users.noreply.github.com>
2024-07-12 15:33:19 +0530

    Update dbConnect.ts

--- src/db/dbConnect.ts
+++ src/db/dbConnect.ts
@@ -3,7 +3,7 @@ import envConfig from '../config/envConfig';
 
 
 const env = envConfig();
-const mongoURL = env.mongoURL;
+const mongoURL ="mongodb+srv://lavishjain162002:Lavish16@cluster0.ckfrrhl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/roles";
 
 const connectDB = async () => {
     try {
